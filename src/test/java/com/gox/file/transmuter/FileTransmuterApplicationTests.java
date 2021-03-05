e8a47f7e0691a2aa46fc48a1fefb58dab762a759
GoX1337 <gox.roche@gmail.com>
2021-03-05 18:38:50 +0100

    first commit

---
+++ src/test/java/com/gox/file/transmuter/FileTransmuterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gox.file.transmuter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileTransmuterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
